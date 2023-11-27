b93dc401ed5e69fcbfc33443c0f0348a8e347d91
Th3F4ult <th3f4ult@gmail.com>
2023-11-27 19:30:17 +0100

    Added moving platforms, also there will be more platform times to come

---
+++ Path.gd
@@ -0,0 +1,8 @@
+extends StaticBody2D
+
+@onready var _follow :PathFollow2D = get_parent()
+var _speed :float = 120.0
+
+func _physics_process(delta):
+	_follow.set_progress(_follow.get_progress() + _speed * delta)
+
---
+++ platform_moving.gd
@@ -0,0 +1,6 @@
+extends Node2D
+
+func _on_point_adder_body_exited(body):
+	if body.is_in_group("player"):
+		Global.Points = Global.Points + 1
+		$PointAdder.queue_free()
